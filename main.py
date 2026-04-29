bd16e6a1ce0057c7abce4c61d1aec9f510991d98
usanglama <42636988+usanglama@users.noreply.github.com>
2026-04-29 14:57:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+API_KEY = os.environ.get("OWM_API_KEY")
+CITY = "Brisbane"
+auth_token = "23a5281f341d3b6711fe87c4c18098f1"
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+parameters = {
+                "q":CITY,
+                "appid":API_KEY,
+                "cnt":4
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+response = requests.get(OWM_Endpoint,params= parameters)
+response.raise_for_status()
+data = response.json()
+check = data['list'][1]['weather'][0]['id']
+count = 0
+will_rain = False
+for c in data['list']:
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+    check = data['list'][count]['weather'][0]['id']
+    if check < 700:
+        print("Its going to rain")
+        will_rain = True
+    else:
+        print("Its not going to rain")
+    count += 1
+
+if will_rain:
+    message = client.messages.create(
+        body = "Its going  to rain today. Remember to bring an ☂️",
+        from_= "+16062499868",
+        to ="+61433748911",
+)
+print(message.status)
+
